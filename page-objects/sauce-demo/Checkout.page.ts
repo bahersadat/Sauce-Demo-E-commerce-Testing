cd6d442ce6531bd5eccc5accc3198f65ddeb8dd4
bahersadat <baher.sadat4@gmail.com>
2022-09-24 16:08:22 +0430

    first realeass of checkout page

--- page-objects/sauce-demo/Checkout.page.ts
+++ page-objects/sauce-demo/Checkout.page.ts
@@ -1,98 +1,108 @@
-import { expect, Locator, Page } from '@playwright/test';
-import { LoginPage } from '../../page-objects/sauce-demo/Login.page';
-import { MainShopPage } from '../../page-objects/sauce-demo/Main-shop.page';
-import { HeaderComponents } from '../../page-objects/sauce-demo/components/primary-header.comp';
-import { CartPage } from '../../page-objects/sauce-demo/Cart.page';
+import { expect, Locator, Page } from "@playwright/test";
+import { LoginPage } from "../../page-objects/sauce-demo/Login.page";
+import { MainShopPage } from "../../page-objects/sauce-demo/Main-shop.page";
+import { HeaderComponents } from "../../page-objects/sauce-demo/components/primary-header.comp";
+import { CartPage } from "../../page-objects/sauce-demo/Cart.page";
 
 var calcTotal = 0;
 
+export class CheckoutPage {
+  // Pages and Components
+  readonly page: Page;
+  readonly loginPage: LoginPage;
+  readonly mainShopPage: MainShopPage;
+  readonly cartPage: CartPage;
+  readonly headerComponents: HeaderComponents;
 
-export class CheckoutPage{
+  // Selectors
+  readonly continueBtn: Locator;
+  readonly continueEmptyErr: Locator;
+  readonly firstName: Locator;
+  readonly lastName: Locator;
+  readonly zipCode: Locator;
+  readonly cancelCheckout: Locator;
+  readonly tax: Locator;
+  readonly summaryTotal: Locator;
+
+  constructor(page: Page) {
     // Pages and Components
-    readonly page:Page;
-    readonly loginPage: LoginPage;
-    readonly mainShopPage: MainShopPage;
-    readonly cartPage: CartPage;
-    readonly headerComponents: HeaderComponents;
+    this.page = page;
+    this.loginPage = new LoginPage(page);
+    this.mainShopPage = new MainShopPage(page);
+    this.cartPage = new CartPage(page);
+    this.headerComponents = new HeaderComponents(page);
 
     // Selectors
-    readonly continueBtn: Locator;
-    readonly continueEmptyErr: Locator;
-    readonly firstName: Locator;
-    readonly lastName: Locator;
-    readonly zipCode: Locator;
-    readonly cancelCheckout: Locator;
-    readonly tax: Locator;
-    readonly summaryTotal: Locator;
-
-    constructor(page:Page){
-        // Pages and Components
-        this.page = page;
-        this.loginPage = new LoginPage(page);
-        this.mainShopPage = new MainShopPage(page);
-        this.cartPage = new CartPage(page);
-        this.headerComponents = new HeaderComponents(page);
-
-        // Selectors
-        this.continueBtn = page.locator('#continue');
-        this.continueEmptyErr = page.locator('h3[data-test="error"]');
-        this.firstName = page.locator('#first-name');
-        this.lastName = page.locator('#last-name');
-        this.zipCode = page.locator('#postal-code');
-        this.cancelCheckout = page.locator('#cancel');
-        this.tax = page.locator('.summary_tax_label');
-        this.summaryTotal = page.locator('.summary_total_label');
-
-    }
-    async clickOnContinueBtn(){
-        await this.continueBtn.click();
-    }
-    async assertEmptyFormSubmission(){
-        await expect(this.continueEmptyErr).toBeVisible();
-    }
-    async fillCheckoutInfo(first_name:string, last_name:string, zip_code:string){
-        await this.firstName.fill(first_name);
-        await this.lastName.fill(last_name);
-        await this.zipCode.fill(zip_code);
-    }
-    async assertCheckoutInfo(){
-        await expect(this.page).toHaveURL('https://www.saucedemo.com/checkout-step-two.html');
-    }
-    async clickOnCancel(){
-        await this.cancelCheckout.click();
-    }
-    async assertCancel(){
-        await expect(this.page).toHaveURL('https://www.saucedemo.com/cart.html');
-    }
-    async SumOfPrices(){
-       const prices = await this.page.$$eval('div.inventory_item_price', (prices) => {
+    this.continueBtn = page.locator("#continue");
+    this.continueEmptyErr = page.locator('h3[data-test="error"]');
+    this.firstName = page.locator("#first-name");
+    this.lastName = page.locator("#last-name");
+    this.zipCode = page.locator("#postal-code");
+    this.cancelCheckout = page.locator("#cancel");
+    this.tax = page.locator(".summary_tax_label");
+    this.summaryTotal = page.locator(".summary_total_label");
+  }
+  async clickOnContinueBtn() {
+    await this.continueBtn.click();
+  }
+  async assertEmptyFormSubmission() {
+    await expect(this.continueEmptyErr).toBeVisible();
+  }
+  async fillCheckoutInfo(
+    first_name: string,
+    last_name: string,
+    zip_code: string
+  ) {
+    await this.firstName.fill(first_name);
+    await this.lastName.fill(last_name);
+    await this.zipCode.fill(zip_code);
+  }
+  async assertCheckoutInfo() {
+    await expect(this.page).toHaveURL(
+      "https://www.saucedemo.com/checkout-step-two.html"
+    );
+  }
+  async clickOnCancel() {
+    await this.cancelCheckout.click();
+  }
+  async assertCancel() {
+    await expect(this.page).toHaveURL("https://www.saucedemo.com/cart.html");
+  }
+  async SumOfPrices() {
+    const prices = await this.page.$$eval(
+      "div.inventory_item_price",
+      (prices) => {
         return prices.map((price) => {
-            const tempPrice =price.textContent;
-            const formatedPrice = el => el.replace('$', ''); 
-            return formatedPrice(tempPrice);
-        })
-       })
-       const formatedPrices = prices.map(Number)
-       
-       const total = formatedPrices.reduce((a,b) => {return a+b;});
-       calcTotal += total;
-        
-       
-    }
-    async calculateTotal(){
-        const TaxValue = (await this.tax.innerText()).valueOf();
-        const tempTax = TaxValue.replace('Tax: $', '');
-        const formatedTax = Number(tempTax);
-        
-        calcTotal += formatedTax; 
-    }
-    async validateTotal() {
-        const summaryTValue = (await this.summaryTotal.innerText()).valueOf();
-        const tempST = summaryTValue.replace('Total: $', '');
-        const formatedST = Number(tempST);
-        console.log('summary total is: '+formatedST+ ' and calculated total is: '+calcTotal);
-        await expect(calcTotal).toEqual(formatedST);
-        
-    }
-    
+          const tempPrice = price.textContent;
+          const formatedPrice = (el) => el.replace("$", "");
+          return formatedPrice(tempPrice);
+        });
+      }
+    );
+    const formatedPrices = prices.map(Number);
+
+    const total = formatedPrices.reduce((a, b) => {
+      return a + b;
+    });
+    calcTotal += total;
+  }
+  async calculateTotal() {
+    const TaxValue = (await this.tax.innerText()).valueOf();
+    const tempTax = TaxValue.replace("Tax: $", "");
+    const formatedTax = Number(tempTax);
+
+    calcTotal += formatedTax;
+  }
+  async validateTotal() {
+    const summaryTValue = (await this.summaryTotal.innerText()).valueOf();
+    const tempST = summaryTValue.replace("Total: $", "");
+    const formatedST = Number(tempST);
+    console.log(
+      "summary total is: " +
+        formatedST +
+        " and calculated total is: " +
+        calcTotal
+    );
+    await expect(calcTotal).toEqual(formatedST);
+  }
 }
--- tests/sauceDemo/checkout.spec.ts
+++ tests/sauceDemo/checkout.spec.ts
@@ -1,61 +1,57 @@
-import { test, expect } from '@playwright/test';
-import { CheckoutPage } from '../../page-objects/sauce-demo/Checkout.page';
+import { test, expect } from "@playwright/test";
+import { CheckoutPage } from "../../page-objects/sauce-demo/Checkout.page";
 
-test.describe('Sauce Demo E-commerce Checkout Page Tests', () => {
-    // declaring variables for Page Objects
-    let checkoutPage: CheckoutPage;
+test.describe("Sauce Demo E-commerce Checkout Page Tests", () => {
+  // declaring variables for Page Objects
+  let checkoutPage: CheckoutPage;
 
-    test.beforeEach(async ({ page }) => {
-        // initializing variables
-        checkoutPage = new CheckoutPage(page);
+  test.beforeEach(async ({ page }) => {
+    // initializing variables
+    checkoutPage = new CheckoutPage(page);
 
-        // essential functions before each test
-        await checkoutPage.loginPage.visit();
-        await checkoutPage.loginPage.login("standard_user", "secret_sauce");
-        await checkoutPage.mainShopPage.addToCart('Sauce Labs Onesie');
-        await checkoutPage.mainShopPage.addToCart('Sauce Labs Backpack');
-        await checkoutPage.mainShopPage.addToCart('Sauce Labs Bolt T-Shirt');
-        await checkoutPage.mainShopPage.clickOnCartComp();
-        await checkoutPage.SumOfPrices();
-        await checkoutPage.cartPage.clickOnCheckoutBtn();
-        await checkoutPage.cartPage.assertCheckoutBtn();
-    });
-    test('Negative scenario - Continue with empty form and assertion', async ({ page }) => {
-        // click continue
-        await checkoutPage.clickOnContinueBtn();
-        // assert emtpy form submission
-        await checkoutPage.assertEmptyFormSubmission();
-    });
-    test('Positive scenario - Continue with filled checkout information', async ({ page }) => {
-        // fill the form
-        await checkoutPage.fillCheckoutInfo('Jhon', 'Smith', '1001');
-        // click on continue
-        await checkoutPage.clickOnContinueBtn();
-        // assert successfull submission
-        await checkoutPage.assertCheckoutInfo();
-    });
-    test('Back to Cart page by canceling the checkout', async ({ page }) => {
-        // click on cancel btn
-        await checkoutPage.clickOnCancel();
-        // assert cancellation
-        await checkoutPage.assertCancel();
-        
-    });
-    test.only('Calculation of total price and validation', async ({ page }) => {
-        // fill checkout info
-        await checkoutPage.fillCheckoutInfo('Jhon', 'Smith', '1001');
-        // click on continue
-        await checkoutPage.clickOnContinueBtn();
-        // calculate total
-        await checkoutPage.calculateTotal();
-        // validate total
-        await checkoutPage.validateTotal();
-
-    })
-
-
-
-
-
-
+    // essential functions before each test
+    await checkoutPage.loginPage.visit();
+    await checkoutPage.loginPage.login("standard_user", "secret_sauce");
+    await checkoutPage.mainShopPage.addToCart("Sauce Labs Onesie");
+    await checkoutPage.mainShopPage.addToCart("Sauce Labs Backpack");
+    await checkoutPage.mainShopPage.addToCart("Sauce Labs Bolt T-Shirt");
+    await checkoutPage.mainShopPage.clickOnCartComp();
+    await checkoutPage.SumOfPrices();
+    await checkoutPage.cartPage.clickOnCheckoutBtn();
+    await checkoutPage.cartPage.assertCheckoutBtn();
+  });
+  test("Negative scenario - Continue with empty form and assertion", async ({
+    page,
+  }) => {
+    // click continue
+    await checkoutPage.clickOnContinueBtn();
+    // assert emtpy form submission
+    await checkoutPage.assertEmptyFormSubmission();
+  });
+  test("Positive scenario - Continue with filled checkout information", async ({
+    page,
+  }) => {
+    // fill the form
+    await checkoutPage.fillCheckoutInfo("Jhon", "Smith", "1001");
+    // click on continue
+    await checkoutPage.clickOnContinueBtn();
+    // assert successfull submission
+    await checkoutPage.assertCheckoutInfo();
+  });
+  test("Back to Cart page by canceling the checkout", async ({ page }) => {
+    // click on cancel btn
+    await checkoutPage.clickOnCancel();
+    // assert cancellation
+    await checkoutPage.assertCancel();
+  });
+  test("Calculation of total price and validation", async ({ page }) => {
+    // fill checkout info
+    await checkoutPage.fillCheckoutInfo("Jhon", "Smith", "1001");
+    // click on continue
+    await checkoutPage.clickOnContinueBtn();
+    // calculate total
+    await checkoutPage.calculateTotal();
+    // validate total
+    await checkoutPage.validateTotal();
+  });
 });
